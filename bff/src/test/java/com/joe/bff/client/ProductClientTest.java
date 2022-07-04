3d335b62c2878b6ecf0e640d86d4b7bdd10b766c
Snigdhajyoti Ghosh <snigdhajyoti.ghosh@gmail.com>
2022-07-05 00:34:40 +0530

    Added different error senarios

--- bff/src/test/java/com/joe/bff/client/ProductClientTest.java
+++ bff/src/test/java/com/joe/bff/client/ProductClientTest.java
@@ -7,27 +7,33 @@ import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.V4Pact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import com.joe.bff.client.model.Product;
+import feign.FeignException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(PactConsumerTestExt.class)
 @PactTestFor(providerName = "producer_service_api", port = "8888")
-@SpringBootTest("feign.client.config.producer-service-api.url: localhost:8888")
+@SpringBootTest(classes = ProductClient.class, properties = "feign.client.config.producer-service-api.url=localhost:8888")
+@EnableFeignClients(clients = ProductClient.class)
+@EnableAutoConfiguration
 class ProductClientTest {
 
     @Autowired
     private ProductClient productClient;
 
     @Pact(provider = "producer_service_api", consumer = "bff_service_api")
-    public V4Pact getProductByIdPact(PactBuilder builder) {
+    public V4Pact productByIdPact(PactBuilder builder) {
         return builder
                 .usingLegacyDsl()
-                .given("provider gives a product with requested id")
-                .uponReceiving("a request to GET a person")
+                .given("Gives a product with requested id: 7")
+                .uponReceiving("A request to GET a product")
                 .path("/products/7")
                 .method("GET")
                 .willRespondWith()
@@ -35,18 +41,42 @@ class ProductClientTest {
                 .matchHeader("Content-Type", "application/json")
                 .body(new PactDslJsonBody()
                         .integerType("id", 7)
-                        .stringValue("name", "Soap")
-                        .stringValue("description", "Germ Protection Bathing Soap Bar")
+                        .stringType("name", "Soap")
+                        .stringType("description", "Germ Protection Bathing Soap Bar")
+                )
+                .toPact(V4Pact.class);
+    }
+
+    @Pact(provider = "producer_service_api", consumer = "bff_service_api")
+    public V4Pact productNotFoundPact(PactBuilder builder) {
+        return builder
+                .usingLegacyDsl()
+                .given("Throws error with invalid id: 8")
+                .uponReceiving("A request to GET a product")
+                .path("/products/8")
+                .method("GET")
+                .willRespondWith()
+                .status(404)
+                .matchHeader("Content-Type", "application/json")
+                .body(new PactDslJsonBody()
+                        .stringType("code", "JOE-404")
+                        .stringType("message", "Product with id 8 not found")
                 )
                 .toPact(V4Pact.class);
     }
 
     @Test
-    @PactTestFor(pactMethod = "getProductByIdPact")
+    @PactTestFor(pactMethod = "productByIdPact")
     public void verifyOkResponse() {
         Product product = productClient.getProduct(7);
         assertEquals(7, product.getId());
         assertEquals("Soap", product.getName());
         assertEquals("Germ Protection Bathing Soap Bar", product.getDescription());
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "productNotFoundPact")
+    public void verifyNotFoundResponse() {
+        assertThrows(FeignException.NotFound.class, () -> productClient.getProduct(8));
     }
 }
